90b28b7150f184b8323b52a42aad894b8f1d5faa
s_lyu <181180083@smail.nju.edu.cn>
2021-03-06 20:47:07 +0800

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=f2e063273b3b8e9cbf2d&state=27eef060-61bd-4e45-8f0a-390132972532

---
+++ 1.两数之和.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> u;
+        for (int i = 0; i < nums.size()-1;++i)
+        {
+            for (int j = i + 1; j < nums.size();++j)
+            {
+                if (nums[i]+nums[j] == target)
+                {
+                    u.push_back(i);
+                    u.push_back(j);
+                }
+            }
+        }
+        return u;
+    }
+};
+// @lc code=end
+
